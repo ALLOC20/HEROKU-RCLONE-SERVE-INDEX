76963df3fd3b7ca5f201fae547142f5e040b5c5a
ALLOC20 <63141816+ALLOC20@users.noreply.github.com>
2021-08-21 23:39:12 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-#author https://github.com/developeranaz (don't delete this)
-curl "https://pastebin.com/raw/bjzadHka" >status
-bash status | grep 'working\|maintenance'
-rclone version
+cd / && wget https://gist.githubusercontent.com/ALLOC20/b51aa087031268cc853654688b33cb94/raw/019dbe46e1c3e66f6fbed87958cdd83001f924d3/.rclone.conf
+rclone serve http onedrive:
