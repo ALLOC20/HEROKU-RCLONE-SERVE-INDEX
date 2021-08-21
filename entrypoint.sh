79d26d667855fdbb9746fea64930826a44e2f943
ALLOC20 <63141816+ALLOC20@users.noreply.github.com>
2021-08-22 00:02:54 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd / && wget $URL
 rclone lsd $CLOUDNAME:
-rclone serve http $CLOUDNAME: --vfs-cache-mode writes --user admin --pass 7273866 --addr :$PORT
+rclone serve webdav $CLOUDNAME: --vfs-cache-mode writes --user admin --pass 7273866 --addr :$PORT
