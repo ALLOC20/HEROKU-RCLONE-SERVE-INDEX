d37fde0dd949ad43719db9fa5dda7cef431cfcb4
ALLOC20 <63141816+ALLOC20@users.noreply.github.com>
2021-08-21 23:54:07 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-cd / && wget https://gist.githubusercontent.com/ALLOC20/b51aa087031268cc853654688b33cb94/raw/019dbe46e1c3e66f6fbed87958cdd83001f924d3/.rclone.conf
-rclone serve http onedrive:
+cd / && wget $URL
+rclone lsd $CLOUDNAME:
+rclone serve http $CLOUDNAME: --vfs-cache-mode writes --user admin --pass 7273866 --addr :$PORT
