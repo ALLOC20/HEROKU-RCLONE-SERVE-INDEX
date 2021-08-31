6f567206e05fd8405e0b31b8f3e99a0b8073d8ab
ALLOC20 <63141816+ALLOC20@users.noreply.github.com>
2021-08-31 19:43:47 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd / && wget $URL
 rclone lsd $CLOUDNAME:
-rclone rcd --rc-web-gui --rc-user=admin --rc-pass=admin --config=/.rclone.conf
+rclone rcd --rc-addr=127.0.0.1:$PORT --rc-web-gui --rc-user=admin --rc-pass=admin --config=/.rclone.conf
